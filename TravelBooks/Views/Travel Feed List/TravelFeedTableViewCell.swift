2c03d63988c50772525c229e68587cb01f84d598
Balaji Galave <balaji.galave@globant.com>
2019-02-05 10:47:15 +0530

    adds comments to  TravelFeedTableViewCell

--- TravelBooks/Views/Travel Feed List/TravelFeedTableViewCell.swift
+++ TravelBooks/Views/Travel Feed List/TravelFeedTableViewCell.swift
@@ -11,6 +11,8 @@ protocol TravelFeedTableViewCellDelegate: AnyObject {
 
 class TravelFeedTableViewCell: UITableViewCell {
     
+    // MARK: IBOutlets
+    
     @IBOutlet private weak var viewBackground: UIView! {
         didSet {
             viewBackground.rounded(10, withBorder: true, borderColor: UIColor.travelFeedCellBorder(), borderWidth: 0.7)
@@ -29,13 +31,23 @@ class TravelFeedTableViewCell: UITableViewCell {
     }
     @IBOutlet private weak var labelDate: UILabel!
     @IBOutlet private weak var imageViewCoverImage: UIImageView!
+    
+    // Height constraint for the for Cover Image
     @IBOutlet private weak var constraintCoverImageHeight: NSLayoutConstraint!
+    
+    // Top space constraint of Cover Image
     @IBOutlet private weak var constraintCoverImageTop: NSLayoutConstraint!
     
+    // Bottom space constraint of Cover Image
     @IBOutlet private  weak var constraintCoverImageBottom: NSLayoutConstraint!
     
+    // Holds reference back to the controller
     weak var delegate: TravelFeedTableViewCellDelegate?
+    
+    // Inde
     var indexPath: IndexPath?
+    
+    // This holds the cover imageView widht accoring to device size
     static var imageViewWidth: CGFloat = 0
     
     override func awakeFromNib() {
@@ -49,12 +61,15 @@ class TravelFeedTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    // Confirgures the cell with give Model
     func configureCell(with model: TravelFeedModel) {
         
+        // Loads user image avatar with url and SDWebImage handles everything
         if let urlUserAvatar = model.userInformation?.urlUserAvatar {
             imageViewUserAvatar.sd_setImage(with: URL(string: urlUserAvatar), completed: nil)
         }
         
+        // Loads Cover Image
         if let urlCoverImageString = model.urlCoverImage,
             let urlCoverImage = URL(string: urlCoverImageString) {
             self.imageViewCoverImage?.sd_setImage(with: urlCoverImage) { [weak self] image, error, _, imageURL in
@@ -72,6 +87,7 @@ class TravelFeedTableViewCell: UITableViewCell {
         setUserNameAndDate(with: model)
     }
     
+    // Sets username and date
     private func setUserNameAndDate(with model: TravelFeedModel) {
         var userName = ""
         if let firstName = model.userInformation?.userFirstName {
@@ -89,19 +105,31 @@ class TravelFeedTableViewCell: UITableViewCell {
         }
     }
     
+    // Gets called when SDWebImage loads the cover image
+    // this calcualates the aspect ration of an image and depending on calculated the height of entire cells
+    // the calcukated height is sent to the controller and tableview reload that particular cell only with update height
     private func loadedCoverImage(with image: UIImage) {
+        
         let imageWidth = imageViewCoverImage.frame.width
+        
         Logger.log(message: "imageViewCover \(String(describing: indexPath?.row)) is = \(imageWidth)",
                    messageType: .debug)
-        let calculatedHeight = (image.size.height * TravelFeedTableViewCell.imageViewWidth) / image.size.width
+        
+        // Get the height of image
+        let calculatedImageHeight = (image.size.height * TravelFeedTableViewCell.imageViewWidth) / image.size.width
+        
         if let indexPath = self.indexPath {
-            let totalCellHeight = calculatedHeight +
+            
+            // Calculate the height og entire cell
+            let totalCellHeight = calculatedImageHeight +
                 constraintCoverImageTop.constant +
                 constraintCoverImageBottom.constant
+            
             Logger.log(message: "Height for the row \(indexPath.row) is = \(totalCellHeight)", messageType: .debug)
             
+            // Update the constraintCoverImageHeight with calculatedImageHeight and reload the cell to fit the image
             DispatchQueue.main.async { [weak self] in
-                self?.constraintCoverImageHeight.constant = calculatedHeight
+                self?.constraintCoverImageHeight.constant = calculatedImageHeight
                 self?.delegate?.reloadRow(at: indexPath, height: totalCellHeight)
             }
         }
